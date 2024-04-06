dbef01424f0d65f5185eb8b2f77afa7cc89367df
ivanpolotskii <66164937+ivanpolotskii@users.noreply.github.com>
2024-04-06 17:42:20 +0300

    changed to react-query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Container from "./components/Container";
-
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 
 function App() {
-
+  const client = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={client}>
       <div className="over"></div>
       <Container/>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,90 +1,67 @@
 import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { observer } from "mobx-react";
 import { weatherTypes } from "../data";
 import formStore from "../FormStore";
-import { observer } from "mobx-react";
 import "../index.scss";
 
-const Container = observer((props) => {
-  const [backgroundImage, setBackgroundImage] = useState(
-    "url(путь_к_вашему_изображению)"
-  );
-  const [currentTime, setCurrentTime] = useState(new Date()); // Добавили состояние для текущего времени
 
-  useEffect(() => {
-    const interval = setInterval(() => setCurrentTime(new Date()), 1000); // Обновляем время каждую секунду
-    return () => clearInterval(interval); // Очищаем интервал при размонтировании компонента
-  }, []);
+const Container = observer(() => {
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-  const tryChangeBackground = (newImageUrl) => {
-    // const img = new Image();
-    setBackgroundImage(`url(${newImageUrl})`);
-    document.body.style.backgroundImage = `url(${newImageUrl})`;
-    // img.onload = () => {
-    //   setBackgroundImage(`url(${newImageUrl})`);
-    //   document.body.style.backgroundImage = `url(${newImageUrl})`;
-    // };
-    // img.onerror = () => {
-    //   console.log("Изображение не найдено, устанавливаем запасной фон");
-    //   document.body.style.backgroundColor = "black";
-    // };
-    // img.src = newImageUrl;
-  };
-
-  const fetchData = () => {
-    axios
-      .get(
-        "https://api.open-meteo.com/v1/forecast?latitude=51.713151&longitude=39.1661&current=weather_code&hourly=temperature_2m,rain,snowfall,cloud_cover,visibility&forecast_days=1"
-      )
-      .then((res) => {
-        const currentHourIndex = new Date().getHours();
-        formStore.setTemp(res.data.hourly.temperature_2m[currentHourIndex]);
-
-        let cloudPercent = res.data.hourly.cloud_cover[currentHourIndex];
-        let rain = res.data.hourly.rain[currentHourIndex];
-        let snowfall = res.data.hourly.snowfall[currentHourIndex];
-
-        if (cloudPercent > 80) formStore.setWeather("clouds");
-        else if (cloudPercent < 80 && cloudPercent > 40)
-          formStore.setWeather("sunclouds");
-        else if (cloudPercent < 40) formStore.setWeather("sun");
-        if (rain > 0) formStore.setWeather("rain");
-        if (snowfall > 0) formStore.setWeather("snowfall");
-      });
-  };
-
-  useEffect(() => {
-    tryChangeBackground(
-      weatherTypes.find((weatherType) => weatherType.type === formStore.weather)
-        ?.img
-    );
-    fetchData();
-    return () => {
-      // document.body.style.backgroundImage = "none";
-    };
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["weatherData"],
+    queryFn: () => {
+      return axios
+        .get(
+          "https://api.open-meteo.com/v1/forecast?latitude=51.713151&longitude=39.1661&current=weather_code&hourly=temperature_2m,rain,snowfall,cloud_cover,visibility&forecast_days=1"
+        )
+        .then((res) => res.data);
+    },
+    onSuccess: (data) => {
+      const currentHourIndex = new Date().getHours();
+      const { temperature_2m, cloud_cover, rain, snowfall } = data.hourly;
+      formStore.setTemp(temperature_2m[currentHourIndex]);
+      determineWeather(cloud_cover[currentHourIndex], rain[currentHourIndex], snowfall[currentHourIndex]);
+    }
   });
 
+  const determineWeather = (cloudPercent, rain, snowfall) => {
+    if (cloudPercent > 80) formStore.setWeather("clouds");
+    else if (cloudPercent > 40) formStore.setWeather("sunclouds");
+    else formStore.setWeather("sun");
+    if (snowfall > 0) formStore.setWeather("snowfall");
+    else if (rain > 0) formStore.setWeather("rain");
+  };
+
+  useEffect(() => {
+    if (data) {
+      const currentHourIndex = new Date().getHours();
+      const { temperature_2m, cloud_cover, rain, snowfall } = data.hourly;
+      formStore.setTemp(temperature_2m[currentHourIndex]);
+      determineWeather(cloud_cover[currentHourIndex], rain[currentHourIndex], snowfall[currentHourIndex]);
+    }
+    const interval = setInterval(() => setCurrentTime(new Date()), 1000);
+    return () => clearInterval(interval);
+    
+  }, [data]);
+
+  if (isLoading) return <div>Loading weather data...</div>;
+  if (isError) return <div>Error fetching weather data: {error.message}</div>;
+
   return (
-    <div className="container">
+    <div className="container" style={{ backgroundImage: `url(${weatherTypes.find(weatherType => weatherType.type === formStore.weather)?.img})` }}>
       <div className="weather-icon">
         {
-          weatherTypes.find(
-            (weatherType) => weatherType.type === formStore.weather
-          )?.icon[+(new Date().getHours() > 4)]
+          weatherTypes.find(weatherType => weatherType.type === formStore.weather)?.icon[+(new Date().getHours() > 4)]
         }
       </div>
       <div className="temperature">{Math.round(formStore.temp)}°C</div>
-      <p>
-        {
-          weatherTypes.find(
-            (weatherType) => weatherType.type === formStore.weather
-          )?.name
-        }
-      </p>
+      <p>{weatherTypes.find(weatherType => weatherType.type === formStore.weather)?.name}</p>
       <div className="city-time">
         <h2>Воронеж</h2>
-        <p>Сейчас: {currentTime.toLocaleTimeString()}</p>{" "}
-        {/* Отображаем текущее время */}
+        <p>Сейчас: {currentTime.toLocaleTimeString()}</p>
       </div>
     </div>
   );
